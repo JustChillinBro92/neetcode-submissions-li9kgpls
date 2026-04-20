24965f126d821465b7984dd9d1deaa408d9c093f
Ayan Purkait <143014625+JustChillinBro92@users.noreply.github.com>
2026-04-20 21:31:16 +0530

    Add: sql-sellers-with-no-sales - submission-2

---
+++ SQL/sql-sellers-with-no-sales/submission-2.sql
@@ -0,0 +1,7 @@
+select s.seller_name
+from seller s
+left join orders o
+    on s.seller_id = o.seller_id
+    and date_part('year', o.sale_date) = 2020
+where o.order_cost is null
+order by s.seller_name asc;
